730595d24c9b1a1cc49cc5740158829d6957979f
octopus <octopus@local>
2026-08-27 16:26:13 +0800

    fix(octopus): emit named exports from vendored react shims

--- packages/octopus/web/src/vendor/jsx-runtime.ts
+++ packages/octopus/web/src/vendor/jsx-runtime.ts
@@ -1,1 +1,6 @@
-export * from "react/jsx-runtime"
+import * as JSXRuntime from "react/jsx-runtime"
+
+export const jsx = JSXRuntime.jsx
+export const jsxs = JSXRuntime.jsxs
+export const jsxDEV = JSXRuntime.jsxDEV
+export const Fragment = JSXRuntime.Fragment
